088a11769922ce34f7ec8b67aab5395e6ded1ce1
Hannes Papenberg <info@joomlager.de>
2022-07-30 15:34:42 +0200

    Adding all necessary repos to config

--- config.js
+++ config.js
@@ -7,5 +7,19 @@ module.exports = {
   enabledManagers: ['composer', 'npm'],
   forkMode: true,
   includeForks: true,
-  repositories: ['joomla-framework/crypt'],
+  repositories: [
+    'joomla/joomla-cms',
+    'joomla/api.joomla.org',
+    'joomla/com_jed4',
+    'joomla/framework.joomla.org',
+    'joomla/help.joomla.org',
+    'joomla/install-from-web-server',
+    'joomla/jissues',
+    'joomla/Manual',
+    'joomla-extensions/jedchecker',
+    'joomla-extensions/patchtester',
+    'joomla-extensions/weblinks',
+    'joomla-projects/custom-elements',
+    'joomla-projects/jorobo'
+  ],
 };
